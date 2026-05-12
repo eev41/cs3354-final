425dce5237cb252b55d203c8c1ef326f8d40b285
AidanTiemann1 <yourfriendlytriop2@gmail.com>
2026-05-12 08:41:39 -0500

    small fix

--- ShippingPolicyFactory.java
+++ ShippingPolicyFactory.java
@@ -1,5 +1,5 @@
 public class ShippingPolicyFactory {
-	public static ShippingStrategy createStrategy(String type) {
+	public static getStrategy createStrategy(String type) {
 		if (type.equalsIgnoreCase("Flat Rate"))
 			return new FlatRateStrategy();
 		if (type.equalsIgnoreCase("Weight-based"))
