797adde3659fa5f503b95ed064ddc9d75f278f79
GrahamLord04 <grahamlord04@gmail.com>
2026-05-12 08:17:41 -0500

    Interface Added!

---
+++ ShippingStrategy.java
@@ -0,0 +1,6 @@
+//Define the Interface: Create an interface named ShippingStrategy 
+//with a single method: double calculate(double weight, double distance).
+
+public interface ShippingStrategy {
+    double calculate(double weight, double distance);
+}
